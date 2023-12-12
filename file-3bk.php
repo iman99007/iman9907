7123d40175d09da0bbed09cea9af2a48a6e4d194
iman99007 <149661282+iman99007@users.noreply.github.com>
2023-12-12 13:43:07 +0330

    Add files via upload

---
+++ file-3bk.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>روبیکا - سامانه رفع فیلتری</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>روبیکا - سامانه رفع فیلتری</h1>
+    </header>
+    <main>
+        <section>
+            <h2>راهنمای سامانه</h2>
+            <p>با سلام خدمت کاربران گرانقدر اپلیشکن روبیکا طرح جدید رفع فیلتری روبیکا آغاز شد در این طرح شما میتوانید فقط با پرداحت 2 هزار تومان اکانت خود را رفع فیلتر نمایید توجه این طرح محدود میباشد پس زودتر اکانت خود را رفع فیلتر نمایید</p>
+        </section>
+        <section>
+            <h2>شماره موبایل فیلتر شده</h2>
+            <p>شماره موبایل: <span id="mobile-number"></span></p>
+        </section>
+        <section>
+            <h2>ایدی اکانت فیلتر شده</h2>
+            <p>ایدی اکانت: <span id="account-id"></span></p>
+        </section>
+        <section>
+            <h2>پرداخت هزینه و رفع فیلتری</h2>
+            <p>هزینه رفع فیلتری: 2 هزار تومان</p>
+            <button id="pay-btn">پرداخت و رفع فیلتری</button>
+        </section>
+    </main>
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ file-qa6.js
@@ -0,0 +1,7 @@
+document.getElementById('mobile-number').textContent = 'موبایل شماره ۱';
+document.getElementById('account-id').textContent = 'ایدی اکانت شماره ۱';
+
+document.getElementById('pay-btn').addEventListener('click', () => {
+    // عملیات پرداخت و رفع فیلتری در اینجا انجام میشود
+    alert('فیلتر شما با موفقیت رفع شد');
+});
